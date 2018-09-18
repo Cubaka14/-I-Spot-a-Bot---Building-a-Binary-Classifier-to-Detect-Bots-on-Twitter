d8109a7affe781e98139c7643c0420cd435e94d9
Nemanja Antic <antic.cubaka@gmail.com>
2018-09-18 03:03:29 +0200

    cresci 2017 full preprocessed data

--- preprocessingScript.py
+++ preprocessingScript.py
@@ -2,21 +2,21 @@ import numpy as np
 import pandas as pd
 
 dfs = []
-dfs.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\fake_followers.csv\fake_followers.csv\users.csv"))
-dfs.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_1.csv\social_spambots_1.csv\users.csv"))
-dfs.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_2.csv\social_spambots_2.csv\users.csv"))
-dfs.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_3.csv\social_spambots_3.csv\users.csv"))
+dfs.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\fake_followers.csv\fake_followers.csv\users.csv"))
+dfs.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_1.csv\social_spambots_1.csv\users.csv"))
+dfs.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_2.csv\social_spambots_2.csv\users.csv"))
+dfs.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_3.csv\social_spambots_3.csv\users.csv"))
 
-dfs.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\traditional_spambots_1.csv\traditional_spambots_1.csv\users.csv"))
-dfs.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\traditional_spambots_2.csv\traditional_spambots_2.csv\users.csv"))
-dfs.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\traditional_spambots_3.csv\traditional_spambots_3.csv\users.csv"))
-dfs.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\traditional_spambots_4.csv\traditional_spambots_4.csv\users.csv"))
+dfs.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\traditional_spambots_1.csv\traditional_spambots_1.csv\users.csv"))
+dfs.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\traditional_spambots_2.csv\traditional_spambots_2.csv\users.csv"))
+dfs.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\traditional_spambots_3.csv\traditional_spambots_3.csv\users.csv"))
+dfs.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\traditional_spambots_4.csv\traditional_spambots_4.csv\users.csv"))
 botsCsv = pd.concat(dfs, sort=True)
 botsCsv['is_bot'] = 1
 
 print("Fake users done!")
 dfs = []
-dfs.append(pd.read_csv(r'C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\genuine_accounts.csv\genuine_accounts.csv\users.csv'))
+dfs.append(pd.read_csv(r'C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\genuine_accounts.csv\genuine_accounts.csv\users.csv'))
 
 print("Human users done!")
 
@@ -38,12 +38,12 @@ del botsCsv
 del humansCsv
 
 dfs2 = []
-dfs2.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\fake_followers.csv\fake_followers.csv\tweets.csv"))
-dfs2.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_1.csv\social_spambots_1.csv\tweets.csv"))
-dfs2.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_2.csv\social_spambots_2.csv\tweets.csv"))
-dfs2.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_3.csv\social_spambots_3.csv\tweets.csv"))
-dfs2.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\traditional_spambots_1.csv\traditional_spambots_1.csv\tweets.csv"))
-dfs2.append(pd.read_csv(r"C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\genuine_accounts.csv\genuine_accounts.csv\tweets.csv"))
+dfs2.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\fake_followers.csv\fake_followers.csv\tweets.csv"))
+dfs2.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_1.csv\social_spambots_1.csv\tweets.csv"))
+dfs2.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_2.csv\social_spambots_2.csv\tweets.csv"))
+dfs2.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\social_spambots_3.csv\social_spambots_3.csv\tweets.csv"))
+dfs2.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\traditional_spambots_1.csv\traditional_spambots_1.csv\tweets.csv"))
+dfs2.append(pd.read_csv(r"C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\genuine_accounts.csv\genuine_accounts.csv\tweets.csv"))
 
 print("Fake tweeters done!")
 
@@ -51,7 +51,7 @@ botsTweets = pd.concat(dfs2, sort=True)
 
 del dfs2
 dfs2 = []
-dfs2.append(pd.read_csv(r'C:\Users\Nemanja.Antic\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\fake_followers.csv\fake_followers.csv/tweets.csv'))
+dfs2.append(pd.read_csv(r'C:\Users\Blagoje\Downloads\cresci2017\cresci-2017.csv\datasets_full.csv\fake_followers.csv\fake_followers.csv/tweets.csv'))
 humansTweets = pd.concat(dfs2, sort=True)
 
 print("Human tweeters done!")
@@ -59,6 +59,19 @@ print("Human tweeters done!")
 resultTweets = pd.concat([botsTweets, humansTweets], sort=True)
 del botsTweets
 del humansTweets
+
+def getFrequencyTweets(listOfDates):
+    x = pd.to_datetime(listOfDates).agg(['min','max'])
+    print(x.max())
+    print(x.min())
+    print(x.max()-x.min())
+    return (x.max() - x.min())/listOfDates.shape[0]
+
+
+# userTweets = resultTweets.query('user_id == 80479674')
+
+# print(getFrequencyTweets(userTweets['timestamp']).total_seconds()/60)
+
 
 def get_mentions_per_tweet(userId,tweets):
     result = {}
@@ -68,6 +81,8 @@ def get_mentions_per_tweet(userId,tweets):
     result['num_urls'] = sum(userTweets['num_urls'])/userTweets.shape[0] if userTweets.shape[0] > 0 else 0
     result['retweet_count'] = sum(userTweets['retweet_count'])/userTweets.shape[0] if userTweets.shape[0] > 0 else 0
     result['favorite_count'] = sum(userTweets['favorite_count'])/userTweets.shape[0] if userTweets.shape[0] > 0 else 0
+    result['frequ_minutes_tweet'] = getFrequencyTweets(userTweets['timestamp']).total_seconds()/60
+    
     return result
 
 men_per_tweet_list =[]
@@ -75,7 +90,9 @@ urls_per_tweet_list =[]
 hashtags_per_tweet_list =[]
 retweets_per_tweet_list =[]
 favs_per_tweet_list =[]
+frequ_minutes_tweet_list = []
 
+index = 0;
 for user in users['id']:
     result = []
     result = get_mentions_per_tweet(user,resultTweets)
@@ -84,7 +101,9 @@ for user in users['id']:
     urls_per_tweet_list.append(result['num_urls'])
     retweets_per_tweet_list.append(result['retweet_count'])
     favs_per_tweet_list.append(result['favorite_count'])
-    print(user)
+    frequ_minutes_tweet_list.append(result['frequ_minutes_tweet'])
+    print(index)
+    index = index + 1
 
 
 users['mentions_per_tweet'] = np.array(men_per_tweet_list)
@@ -92,9 +111,11 @@ users['hashtags_per_tweet'] = np.array(hashtags_per_tweet_list)
 users['urls_per_tweet'] = np.array(urls_per_tweet_list)
 users['retweets_per_tweet'] = np.array(retweets_per_tweet_list)
 users['favorites_per_tweet'] = np.array(favs_per_tweet_list)
+users['frequ_minutes_tweet'] = np.array(frequ_minutes_tweet_list)
 
 writer = pd.ExcelWriter('cresci2017Preprocessed.xlsx', engine='xlsxwriter')
 users.to_excel(writer, sheet_name='Sheet1')
 writer.save()
 
 users.to_csv('cresci2017Preprocessed.csv')
+
